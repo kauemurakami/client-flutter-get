852603c2e03a5a4e1aeba223520031af74388b33
Kauê Murakami <kauetmurakami@gmail.com>
2020-06-01 12:07:30 -0300

    upload inicial

---
+++ android/app/src/main/kotlin/com/example/client_deno/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.client_deno
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/model/client.dart
@@ -0,0 +1,11 @@
+
+
+class Client {
+  
+  String id;
+  String name;
+  String email;
+  String password;
+
+  Client({this.id, this.name, this.email, this.password});
+}
